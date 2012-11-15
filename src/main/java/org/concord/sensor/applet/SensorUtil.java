a96555d214959624500bcff1224d5166c3606dfe
Aaron Unger <aaron.unger@gmail.com>
2012-11-15 09:50:20 -0800

    Replace code incompatible with java 1.5

--- src/main/java/org/concord/sensor/applet/SensorUtil.java
+++ src/main/java/org/concord/sensor/applet/SensorUtil.java
@@ -1,8 +1,6 @@
 package org.concord.sensor.applet;
 
 import java.applet.Applet;
-import java.awt.EventQueue;
-import java.util.Arrays;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
@@ -90,7 +88,8 @@ public class SensorUtil {
 				try {
 					final int numSamples = device.read(data, 0, 1, null);
 					if(numSamples > 0) {
-						final float[] dataCopy = Arrays.copyOfRange(data, 0, numSamples);
+						final float[] dataCopy = new float[numSamples];
+						System.arraycopy(data, 0, dataCopy, 0, numSamples);
 						executor.schedule(new Runnable() {
 							public void run() {
 								jsBridge.handleData(numSamples, dataCopy);
